b29e120e227ce699f481c9d6502573180eca5c1f
lucascordoba7 <lucascordoba77@gmail.com>
2021-08-30 12:42:26 -0300

    Genre exceptions handled

--- src/main/java/com/lucascordoba/backendchallenge/repositories/GenreRepository.java
+++ src/main/java/com/lucascordoba/backendchallenge/repositories/GenreRepository.java
@@ -3,5 +3,9 @@ package com.lucascordoba.backendchallenge.repositories;
 import com.lucascordoba.backendchallenge.models.GenreModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GenreRepository extends JpaRepository<GenreModel,Long> {
+    Optional<GenreModel> findByName(String name);
+
 }
